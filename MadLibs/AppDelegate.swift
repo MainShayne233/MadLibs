a8ffc732e1991c2601c67a91b975114eef0b45d1
Shayne <shaynetremblay@hotmail.com>
2015-08-23 16:43:15 -0400

    Initial Commit

---
+++ MadLibs/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MadLibs
+//
+//  Created by shayne tremblay on 8/23/15.
+//  Copyright (c) 2015 Open Sourcery. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
